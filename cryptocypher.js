24948541ccc82b50ac4b20c901ea578dfd0ee163
manidharchowdarynarra <110219877+manidharchowdarynarra@users.noreply.github.com>
2022-10-21 12:02:46 +0530

    Add files via upload

---
+++ cryptocypher.js
@@ -0,0 +1,5 @@
+var crypto = require('crypto');
+var mykey = crypto.createCipher('aes-128-cbc', 'mypassword');
+var mystr = mykey.update('abc', 'utf8', 'hex')
+mystr += mykey.final('hex');
+console.log(mystr);
